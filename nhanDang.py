5833cafad33457165e2b8e45ed4fc87943b54068
DoanNV PC <doannv19@gmail.com>
2019-03-15 02:04:47 +0700

    het bai 11 commit chia 1 anh thanh nhieu anh nho va chuyen mang 2 chieu thanh 1 chieu

--- nhanDang.py
+++ nhanDang.py
@@ -1,10 +1,17 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-img=cv2.imread('digits.png',0)
-#cv2.imshow('tes',img)
 
+img=cv2.imread('digits.png',0)
+#cat 1 anh thanh nhieu mang nho
 cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
-print(cells[0][0])
+
+cv2.imwrite('anhso.jpg',cells[0][0])
+#chuyen mang sang numpy
+x = np.array(cells)
+#chia thanh mang 1 chieu moi chieu 20 pixel
+xx = x[0,0].reshape(-1,20)
+print(xx)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
