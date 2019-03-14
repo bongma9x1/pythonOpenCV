206ddfe2d7ea7eca36663afba3cf7447a996140e
DoanNV PC <doannv19@gmail.com>
2019-03-15 02:22:18 +0700

    xong phai 12 su ly nhan dang 1 so

--- nhanDang.py
+++ nhanDang.py
@@ -6,12 +6,21 @@ img=cv2.imread('digits.png',0)
 #cat 1 anh thanh nhieu mang nho
 cells = [np.hsplit(row,100) for row in np.vsplit(img,50)]
 
-cv2.imwrite('anhso.jpg',cells[0][0])
-#chuyen mang sang numpy
 x = np.array(cells)
-#chia thanh mang 1 chieu moi chieu 20 pixel
-xx = x[0,0].reshape(-1,20)
-print(xx)
+#tao du lieu traning va du lieu test
+train = x[:,:50].reshape(-1,400).astype(np.float32)
+test = x[:,50:100].reshape(-1,400).astype(np.float32)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+#gan nhan cho du lieu train
+k = np.arange(10)
+train_labels = np.repeat(k,250)[:,np.newaxis]
+
+#nhan dang
+knn = cv2.ml.KNearest_create()
+knn.train(train,0,train_labels)
+kq1,kq2,kq3,kq4 = knn.findNearest(test,5)
+print(kq2)
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
